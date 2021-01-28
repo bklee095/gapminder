a9c6abd9eebf9a93da0646ef4fc497c67f3ab187
bklee095 <beomkilee18@gmail.com>
2021-01-27 21:22:32 -0500

    Update gapminder_1.R

--- gapminder_1.R
+++ gapminder_1.R
@@ -23,11 +23,19 @@ ggplot(gp_1952, aes(x = gdpPercap, y = lifeExp, color = continent)) +
 gp_2007 <- gapminder %>%
   filter(year == "2007")
 
-
+#additional features
 ggplot(gp_2007, aes(x = gdpPercap, y = lifeExp, color = continent, size = pop)) +
   geom_point() + scale_x_log10()
-
 
 #faceting over years
 ggplot(gapminder, aes(x = gdpPercap, y = lifeExp, color = continent, size = pop)) +
   geom_point() + facet_wrap(~year) + scale_x_log10()
+
+
+#Yrly life expectancy trend over time
+by_year <- gp %>%
+  group_by(year) %>%
+  summarize(medLifeExp = median(lifeExp), maxGDPpercap = max(gdpPercap)) 
+
+ggplot(by_year, aes(x = year, y = medLifeExp)) +
+  geom_point() + expand_limits(y = 0)
